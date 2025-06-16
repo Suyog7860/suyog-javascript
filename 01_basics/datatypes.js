924f423923cc29da34efea406a7623f4ef90a8a1
Suyog Ganesh Bhalerav <118674443+Suyog7860@users.noreply.github.com>
2025-06-16 14:33:30 +0000

    container config done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,4 @@
+"use strict"; // treat all JS code as newer version of JS
+// alert(3+8) // We are using nodejs, nodejs not browser console
+console.log(3 + 3);
+console.log("Suyog");    //code readability should be high
---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+var a = 10;
+let b = 20;
+const c = 100;
+console.log(a+b);
+console.log(a+c);
+
+console.log(c+b);
+
