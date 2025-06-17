53f1bdaa6ea5574a8c2482f347c20e62a2e93bac
Suyog Ganesh Bhalerav <118674443+Suyog7860@users.noreply.github.com>
2025-06-17 04:56:14 +0000

    datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -2,3 +2,12 @@
 // alert(3+8) // We are using nodejs, nodejs not browser console
 console.log(3 + 3);
 console.log("Suyog");    //code readability should be high
+let a = "Suyog";
+console.log(typeof a);
+console.log(typeof "Suyog");
+console.log(typeof null);
+console.log(typeof object);
+console.log(typeof undefined);
+
+
+
