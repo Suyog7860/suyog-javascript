ec255d44642897192de0384129482779fc921740
Suyog Ganesh Bhalerav <118674443+Suyog7860@users.noreply.github.com>
2025-06-17 11:08:27 +0000

    data types summary

---
+++ 01_basics/datatypesummary.js
@@ -0,0 +1,33 @@
+// primitive data types in js
+// String, Number, Boolean, null, undefined, symbol, BigInt.
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+console.log(anotherId);
+
+const bigNumber = 78965423215932n
+
+
+
+
+// reference/Non-Primitive data types in js
+// arrays, objects, functions
+
+
+const heros = ["Shaktiman", "Nagraj", "Krish"];
+let myObj = {
+    name: "Suyog",
+    age:24,
+}
+
+
+const myFunction = function(){
+    console.log("Hello Suyog");
+    
+}
