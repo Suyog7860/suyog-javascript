1e1830c02b187fcecc4c4680d06bef325086e8d9
Suyog Ganesh Bhalerav <118674443+Suyog7860@users.noreply.github.com>
2025-06-17 05:20:26 +0000

    conversion of datatyes

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,21 @@
+// let score = "true"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+// let isLoggedIn = 1
+// let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn);
+// console.log(typeof booleanLoggedIn);
+
+let someNumber = 33
+let StrinNumber = String(someNumber)
+console.log(StrinNumber);
+console.log(typeof StrinNumber);
