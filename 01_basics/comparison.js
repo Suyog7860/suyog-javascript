7ac76d0ac6c75d2f60c506ee81c96ee2373a6cf6
Suyog Ganesh Bhalerav <118674443+Suyog7860@users.noreply.github.com>
2025-06-17 10:38:28 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,8 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// avoidable
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(undefined == 0);
