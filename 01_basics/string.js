f9c7c38845924ada4c6aee040504b52e988aa846
Suyog Ganesh Bhalerav <118674443+Suyog7860@users.noreply.github.com>
2025-06-18 17:42:59 +0000

    nums & maths operations

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -4,12 +4,12 @@ const repoCount = 10
 console.log(`Hello my name is ${name} and my Github repository count is ${repoCount}`);
 
 const gameName = new String('Ssgb');
-console.log(gameName[0]);
-console.log(gameName.__proto__);
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('g'));
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('g'));
 
 const newString = gameName.substring(0,4)
 console.log(newString);
@@ -18,6 +18,13 @@ const anotherString = gameName.slice(-2,3)
 console.log(anotherString);
 
 
+const newStringOne = " Suyog "
+console.log(newStringOne);
+console.log(newStringOne.trim());
 
+const url = "https://google.com/suyog%30bhalerav"
+console.log(url.replace('%30','-'));
 
+console.log(url.includes('suyog'));
 
+console.log(gameName.split('-'));
