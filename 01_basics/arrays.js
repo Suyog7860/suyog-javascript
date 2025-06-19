53d35277a9ce13af856c40f4bbb9819ee0018673
Suyog Ganesh Bhalerav <118674443+Suyog7860@users.noreply.github.com>
2025-06-19 18:09:04 +0000

    array config done

---
+++ 01_basics/arrays.js
@@ -0,0 +1,9 @@
+// arrays
+
+const myArr = [0,1,2,3,4,5,]
+console.log(myArr[0]);
+console.log(myArr[1]);
+console.log(myArr[2]);
+console.log(myArr[3]);
+console.log(myArr[4]);
+console.log(myArr[5]);
